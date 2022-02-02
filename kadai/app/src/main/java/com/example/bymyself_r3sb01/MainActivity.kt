47e1bd35055861085090f76ec8148769227a2b41
mossan0706 <mossanaoki@gmail.com>
2022-02-02 09:43:32 +0900

    課題！

--- kadai/app/src/main/java/com/example/bymyself_r3sb01/MainActivity.kt
+++ kadai/app/src/main/java/com/example/bymyself_r3sb01/MainActivity.kt
@@ -2,15 +2,10 @@ package com.example.bymyself_r3sb01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.bymyself_r3sb01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private  lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
